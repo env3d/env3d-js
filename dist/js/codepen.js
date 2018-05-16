1d815be7789b2f4f2fe4477590e8820fcbff9291
Jason Madar <jason@vandrico.com>
2018-05-15 23:31:25 -0700

    codepen init to include lighting

--- dist/js/codepen.js
+++ dist/js/codepen.js
@@ -5,6 +5,21 @@ env3d.Env.baseAssetsUrl = "https://env3d.github.io/env3d-js/dist/";
 
 // Start env
 var env = new env3d.Env();
+
+// lighting
+env.scene.add(new THREE.HemisphereLight( 0x7f7f7f, 0x000000, 10 ));
+
+[
+    {x: -4, y: -4, z: -4},
+    {x: 1, y: 2, z: 1}
+].map( p => {
+    var light = new THREE.DirectionalLight( 0xffffff);
+    light.position.set( p.x, p.y, p.z ).normalize();
+    return light;
+}).forEach ( l => {
+    env.scene.add( l );
+});                 
+
 env.start();
 
 // Allow VR to be used
